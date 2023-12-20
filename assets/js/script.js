feaabbe8c1f0510abcf9cef55853e8a50f965e8b
Frederic Lannes <lannes.frederic@orange.fr>
2023-12-20 14:26:10 +0100

    fin projet avec ouverture fixe des box + arret musique

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
+//  sauvegarder les cases cliquées dans le navigateur (session storage , localstorage)
+
+
 import quotes from "./quotes.js"
 
 const boxes = document.querySelectorAll(".js_box");
 const today = new Date(Date.now());
 const dateNumber = today.getDate();
 
-
+const showPreviouslyOpenedBoxes = () => {
+    const openedBoxes = localStorage.getItem("openedBoxes")
+    if(openedBoxes !== null){
+        const listOfBoxes = openedBoxes.split(",")
+        for(let i=0; i<listOfBoxes.length; i++){
+            const boxNumber = listOfBoxes[i]
+            const box = document.querySelector(`[data-number="${boxNumber}"]`)
+            showImage(box)
+        }
+    }
+}
 
 boxes.forEach(box => {
     box.addEventListener("click", () => {
@@ -13,6 +26,7 @@ boxes.forEach(box => {
             playSong();
             showImage(box);
             openModal(boxNumber)
+            saveHistory(boxNumber)
         }
     })
 });
@@ -25,17 +39,34 @@ const playSong = () => {
     song.pause()
     song.currentTime = 0
     song.play()
+    
 };
 
 const showImage = (boxToHide) => {
     boxToHide.classList.add("hide");
 }
 
-    const modal = document.querySelector(".js-modal");
-    const quote = document.querySelector(".js-quote");
-    const author = document.querySelector(".js-author")
-    const openModal = (index) => {
-        quote.textContent = quotes[index].quote
-        author.textContent = quotes[index].author
-        modal.showModal()
-    }
+const modal = document.querySelector(".js-modal");
+const quote = document.querySelector(".js-quote");
+const author = document.querySelector(".js-author");
+const openModal = (index) => {
+    quote.textContent = quotes[index].quote
+    author.textContent = quotes[index].author
+    modal.showModal()
+};
+
+modal.addEventListener("close", () => {
+    song.pause();
+});
+
+const saveHistory = (boxNumber) => {
+    let openedBoxes = []
+    let localValue = localStorage.getItem("openedBoxes")
+    if (localValue != null) {
+        openedBoxes= localValue.split(',') 
+    } 
+        openedBoxes.push(boxNumber)
+        localStorage.setItem("openedBoxes", openedBoxes)
+};
+
+showPreviouslyOpenedBoxes()
